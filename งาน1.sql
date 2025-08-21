ade505c023301f0b594d12b7cd109220c9e43411
thegig009 <loveesesgig2005@gmail.com>
2025-08-21 10:10:41 +0700

    Update งาน1.sql

--- งาน1.sql
+++ งาน1.sql
@@ -43,3 +43,32 @@ from Orders JOIN Shippers
 on Shippers.ShipperID = Orders.ShipVia
 AND OrderID = 10275
 
+-- ต้องการหารหัสพนักงาน ชื่อพนักงาน รหัสใบสั้งซื้อที่เกั่ยวข้อง เรียงต่มลำดับรหัสนักงาน 
+-- ต้องการรหัสพนักงาน เมือง เเละประเทศของบริษัทผู็จำหน่าย 
+-- ต้องการชื่อบริษัทข่นส่ง เเละจำนวนใบสั่่งซื้อที่เกียวข้อง 
+-- ต้องการหร้สสินค้า ชื่อสินค้า เเละจำนวนทังหมดที่ขายได้ 
+
+select e.EmployeeID , FirstName , OrderID
+from Employees as e JOIN Orders as o
+on e.EmployeeID = o.EmployeeID
+order by EmployeeID
+
+select ProductID ,ProductName , City , Country
+from Products p join Suppliers s 
+on p.SupplierID = s.SupplierID
+
+
+select CompanyName , count(*)
+from Orders as o join Shippers as s 
+on o.ShipVia = s.ShipperID
+group by CompanyName
+
+
+select p.ProductID,p.ProductName , sum(Quantity) as จำนานที่ขายทั้งหมด
+from Products p  join [Order Details] od 
+on  p.ProductID = od.ProductID
+group by p.ProductID , p.ProductName
+order by 1
+
+
+
